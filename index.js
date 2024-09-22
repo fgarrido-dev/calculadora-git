662116a13f2687a5f3a9767be9b4ab9feb9c8cbe
fgarrido-dev <apenasmaisumacontado@gmail.com>
2024-09-22 16:51:40 -0300

    cria variavel resultado e switch case com operacoes

--- index.js
+++ index.js
@@ -7,4 +7,32 @@ function calculadora() {
     let n2 = parseFloat(prompt("Insira o segundo número:"));
 
     let operacao = prompt("Qual operação deseja realizar? Digite: 1 - Soma | 2 - Subtração | 3 - Multiplicação | 4 - Divisão.");
-}t
+
+    let resultadoDaOperacao;
+    
+    switch(operacao) {
+        case "1":
+            resultadoDaOperacao = n1 + n2;
+            break;
+        case "2":
+            resultadoDaOperacao = n1 - n2;
+            break;
+        case "3":
+            resultadoDaOperacao = n1 * n2;
+            break;
+        case "4":
+            if (n2 !== 0) {
+                resultadoDaOperacao = n1 / n2;
+            } else {
+                alert("Não é possível dividir um número por zero!");
+                calculadora();
+                return;
+            }
+            break;
+        default:
+            alert("Operação inválida!");
+            calculadora();
+            return;
+    }
+
+}
