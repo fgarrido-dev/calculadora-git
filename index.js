a3c8d6cc9eef062899b5bd7b15090322c31864a9
fgarrido-dev <apenasmaisumacontado@gmail.com>
2024-09-22 16:49:40 -0300

    cria funcao calculadora, alerta, declaracoes e prompts

---
+++ index.js
@@ -0,0 +1,10 @@
+function calculadora() {
+    
+    alert("Hello World, isso é uma calculadora!");
+    
+    let n1 = parseFloat(prompt("Insira o primeiro número"));
+
+    let n2 = parseFloat(prompt("Insira o segundo número:"));
+
+    let operacao = prompt("Qual operação deseja realizar? Digite: 1 - Soma | 2 - Subtração | 3 - Multiplicação | 4 - Divisão.");
+}t
